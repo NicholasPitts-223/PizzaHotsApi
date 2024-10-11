1623a7c5a8efd0a6bb56d28cf1547d820d36b44e
Nicholas J S Pitts <142807894+NicholasPitts-223@users.noreply.github.com>
2024-10-11 18:58:35 -0300

    Add files via upload

---
+++ PizzaHotApi/Models/Pizza.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace PizzaHotApi.Models;
+
+public class Pizza
+{
+
+    public int Id { get; set; }
+
+    public string? Nome { get; set; }
+
+    public string? descrição { get; set; }
+
+    public int Quantidade { get; set; }
+
+}
+
+
+
